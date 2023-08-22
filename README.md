530f0dbc8df61a7f8d8811c32fa14468c2e633d6
Pedro Martin del Campo Gonzalez <pedro.martindelcampo.gonzalez@gmail.com>
2023-08-22 11:55:58 -0700

    Create main window for the game

---
+++ README.md
@@ -0,0 +1,15 @@
+# Treasure Adventure
+
+Treasure adventure is a videogame created with in Java 8 and Maven.
+
+## Creating a new build
+
+```sh
+mvn clean package
+```
+
+## Running the videogame
+
+```sh
+java -jar target/treasure-adventure-1.0-SNAPSHOT-jar-with-dependencies.jar
+```
--- src/main/java/com/pmg/treasure/App.java
+++ src/main/java/com/pmg/treasure/App.java
@@ -1,9 +1,17 @@
 package com.pmg.treasure;
 
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
 public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Treasure Adventure");
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }
